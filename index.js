74ea0fcf0759f961a1a773fdb56c05a4d1e92fd4
Dewashish Gope <dewashishgope99@gmail.com>
2024-12-19 01:48:41 +0530

    Dashboard project with Javascript Html and CSS

---
+++ index.js
@@ -0,0 +1,19 @@
+const sideMenu = document.getElementById("aside");
+const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn.addEventListener('click' , () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
+
+themeToggler.addEventListener('click', () => {
+
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span').classList.toggle('active');
+})
